0fad27279ad4b12f20397ccce814cbd40306fbb7
Balen <idrisrasheed2000@gmail.com>
2022-07-25 22:59:11 +0100

    update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { ListGroup } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 import { AppContext } from '../context/appContext';
@@ -6,12 +6,29 @@ import { AppContext } from '../context/appContext';
 function Sidebar() {
   const user = useSelector((state) => state.user);
   const { socket, setMembers, members, setCurrentRoom, setRooms, privateMemberMsg, rooms, setPrivateMemberMsg, currentRoom  } = useContext(AppContext);
+ 
+   useEffect(() => {
+  if (user) {
+    setCurrentRoom('general');
+    getRooms();
+    socket.emit('join-room', 'general');
+    socket.emit('new-user');
+  }
+   }, []) 
+ 
   socket.off('new-user').on('new-user', (payload) => {
     setMembers(payload);
   });   
-   if ( !user ) {
-    return <div></div>
-   }
+
+function getRooms(){
+   fetch('http://localhost:5001/rooms')
+   .then((res) => res.json())
+   .then(data => setRooms(data))
+}
+
+if ( !user ) {
+  return <div></div>
+ }
  return (
   <>
     <h2>Available rooms</h2>
